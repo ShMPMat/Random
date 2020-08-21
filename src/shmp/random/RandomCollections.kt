19f15ff20ef9d08aacc20a7ef8f033b9feee4b06
ShMP <matveyshnytkin@gmail.com>
2020-08-21 17:45:11 +0300

    Faster inlined functions; removed Collections support for the random stability.

--- src/shmp/random/RandomCollections.kt
+++ src/shmp/random/RandomCollections.kt
@@ -3,33 +3,64 @@ package shmp.random
 import kotlin.random.Random
 
 
-fun <E> randomElement(collection: Collection<E>, random: Random) = collection.toList()[random.nextInt(collection.size)]
+fun <E> randomElement(list: List<E>, random: Random) = list[random.nextInt(list.size)]
 
-fun <E> randomElement(collection: Collection<E>, mapper: (E) -> Double, random: Random): E {
-    val list = collection.toList()
+
+inline fun <E> randomElement(list: List<E>, mapper: (E) -> Double, random: Random): E {
     val probabilities = list.map(mapper)
-    var result = random.nextDouble() * probabilities.fold(0.toDouble(), Double::plus)
+    var result = random.nextDouble() * probabilities.fold(0.0, Double::plus)
+
     for (i in probabilities.indices) {
         val probability = probabilities[i]
-        if (result <= probability) {
+
+        if (result <= probability)
             return list[i]
-        }
+
         result -= probability
     }
+
     throw RandomException("Can't choose an element from an empty collection")
 }
+
+inline fun <E> randomElement(iterable: Iterable<E>, mapper: (E) -> Double, random: Random): E {
+    val probabilities = iterable.map(mapper)
+    var result = random.nextDouble() * probabilities.fold(0.0, Double::plus)
+
+    for ((i, element) in iterable.withIndex()) {
+        val probability = probabilities[i]
+
+        if (result <= probability)
+            return element
+
+        result -= probability
+    }
+
+    throw RandomException("Can't choose an element from an empty collection")
+}
+
+inline fun <E> randomElement(array: Array<E>, mapper: (E) -> Double, random: Random): E {
+    val probabilities = array.map(mapper)
+    var result = random.nextDouble() * probabilities.fold(0.0, Double::plus)
+
+    for (i in probabilities.indices) {
+        val probability = probabilities[i]
+
+        if (result <= probability)
+            return array[i]
+
+        result -= probability
+    }
+
+    throw RandomException("Can't choose an element from an empty collection")
+}
+
 
 fun <E : SampleSpaceObject> randomElement(array: Array<E>, random: Random): E =
     randomElement(array, { it.probability }, random)
 
-fun <E : SampleSpaceObject> randomElement(collection: Collection<E>, random: Random): E =
-    randomElement(collection, { it.probability }, random)
+fun <E : SampleSpaceObject> randomElement(list: List<E>, random: Random): E =
+    randomElement(list, { it.probability }, random)
 
-fun <E> randomElement(array: Array<E>, mapper: (E) -> Double, random: Random): E =
-    randomElement(array.toList(), mapper, random)
-
-fun <E> randomElement(iterable: Iterable<E>, mapper: (E) -> Double, random: Random): E =
-    randomElement(iterable.toList(), mapper, random)
 
 fun <E> randomSublist(list: List<E>, random: Random, min: Int = 0, max: Int = list.size) =
     list.shuffled(random).subList(0, random.nextInt(min, max))
@@ -44,20 +75,20 @@ fun <E> randomSublist(
     randomSublist(array.toList(), mapper, random, min, max)
 
 fun <E> randomSublist(
-    collection: Collection<E>,
+    list: List<E>,
     mapper: (E) -> Double,
     random: Random,
     min: Int = 0,
-    max: Int = collection.map(mapper).count { it > 0.0 }
+    max: Int = list.map(mapper).count { it > 0.0 }
 ): List<E> {
-    if (collection.isEmpty())
+    if (list.isEmpty())
         throw RandomException("Can't choose an element from an empty collection")
     val resultList = ArrayList<E>()
     val size = random.nextInt(min, max)
-    val list = collection.toList()
     val probabilities = list.map(mapper).toMutableList()
-    val probabilitySum = probabilities.fold(0.toDouble(), Double::plus)
+    val probabilitySum = probabilities.fold(0.0, Double::plus)
     var sum = random.nextDouble() * probabilitySum
+
     for (i in 1..size) {
         var currentSum = sum
         for (j in probabilities.indices) {
