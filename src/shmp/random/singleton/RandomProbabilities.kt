b1524456edc141f2c1635da368847deff8d5b6aa
ShMP <matveyshnytkin@gmail.com>
2021-03-04 19:05:57 +0300

    A new flow-control function.

--- src/shmp/random/singleton/RandomProbabilities.kt
+++ src/shmp/random/singleton/RandomProbabilities.kt
@@ -4,3 +4,10 @@ import shmp.random.testProbability
 
 
 fun Double.testProbability() = testProbability(this, RandomSingleton.random)
+
+fun Double.chanceOf(block: () -> Unit) {
+    if (this.testProbability())
+        block()
+}
+
+
