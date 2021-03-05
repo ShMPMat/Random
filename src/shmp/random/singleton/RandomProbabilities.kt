a3dcc8788c3bc910a3a3548ec5afa74895859083
ShMP <matveyshnytkin@gmail.com>
2021-03-05 12:42:10 +0300

    More flow control! Now with null-safety and else-like branch.

--- src/shmp/random/singleton/RandomProbabilities.kt
+++ src/shmp/random/singleton/RandomProbabilities.kt
@@ -1,16 +1,38 @@
 package shmp.random.singleton
 
+import shmp.random.singleton.Success.*
 import shmp.random.testProbability
 
 
 fun Double.testProbability() = testProbability(this, RandomSingleton.random)
 
-inline fun Double.chanceOf(block: () -> Unit) {
-    if (this.testProbability())
+inline fun Double.chanceOf(block: () -> Unit): Success {
+    return if (this.testProbability()) {
+        block()
+        SUCCESS
+    } else FAILURE
+}
+
+inline fun Double.chanceOfNot(block: () -> Unit): Success {
+    if (!this.testProbability()) {
+        block()
+        return SUCCESS
+    }
+    return FAILURE
+}
+
+inline infix fun Success.otherwise(block: () -> Unit) {
+    if (!isSuccess)
         block()
 }
 
-inline fun Double.chanceOfNot(block: () -> Unit) {
-    if (!this.testProbability())
+inline fun <E> Double.chanceOf(block: () -> E): E? {
+    return if (this.testProbability()) {
         block()
+    } else null
+}
+
+enum class Success(val isSuccess: Boolean) {
+    SUCCESS(true),
+    FAILURE(false)
 }
